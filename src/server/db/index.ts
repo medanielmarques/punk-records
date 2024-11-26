387d0639a19ab925edb92f1a9dff89df88ad8bc0
Daniel Marques <daniel.brz2009@gmail.com>
2024-11-26 15:25:25 -0300

    yeah

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,18 @@
-import { env } from "@/env"
-import { type Client, createClient } from "@libsql/client"
-import { drizzle } from "drizzle-orm/libsql"
+// import { env } from "@/env"
+// import { type Client, createClient } from "@libsql/client"
+// import { drizzle } from "drizzle-orm/libsql"
 
-import * as schema from "./schema"
+// import * as schema from "./schema"
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  client: Client | undefined
-}
+// /**
+//  * Cache the database connection in development. This avoids creating a new connection on every HMR
+//  * update.
+//  */
+// const globalForDb = globalThis as unknown as {
+//   client: Client | undefined
+// }
 
-export const client = globalForDb.client ?? createClient({ url: "" })
-if (env.NODE_ENV !== "production") globalForDb.client = client
+// export const client = globalForDb.client ?? createClient({ url: "" })
+// if (env.NODE_ENV !== "production") globalForDb.client = client
 
-export const db = drizzle(client, { schema })
+// export const db = drizzle(client, { schema })
